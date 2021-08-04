d92f093a330f1b45a7bb62762e641170f0eb80e5
Jchu4 <jerome.chuame@gmail.com>
2021-08-04 23:33:18 +0800

    Download relevant libaries, base setup of files

---
+++ src/components/Upload.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button, FileUpload } from "carbon-components-react";
+
+export default function Upload() {
+  return (
+    <>
+      <Button>Upload</Button>
+    </>
+  );
+}
