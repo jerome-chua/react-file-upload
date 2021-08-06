b6ef08bb63df8d52877c0e53eb4703456fc6c073
Jchu4 <jerome.chuame@gmail.com>
2021-08-06 09:44:18 +0800

    Include bootstrap elements and relevant libraries

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { Container, Row, Table } from "react-bootstrap";
 import Upload from "./Upload.jsx";
 
 export default function App() {
   return (
-    <div>
-      <h1>File Upload</h1>
-      <Upload />
-    </div>
+    <Container>
+      <Row>
+        <h1>File Upload</h1>
+        <Upload />
+      </Row>
+      <Row>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>File Name</th>
+              <th>Modified</th>
+            </tr>
+          </thead>
+        </Table>
+      </Row>
+    </Container>
   );
 }
